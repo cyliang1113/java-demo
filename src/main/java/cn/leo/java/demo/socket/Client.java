3465f69a9910d06668196912f4012ab6d84da1b0
cyliang1113 <cyliang1113@126.com>
2020-04-19 19:56:25 +0800

    11111

--- src/main/java/cn/leo/java/demo/socket/Client.java
+++ src/main/java/cn/leo/java/demo/socket/Client.java
@@ -3,6 +3,7 @@ package cn.leo.java.demo.socket;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -10,19 +11,29 @@ public class Client {
 		Socket socket = null;
 		OutputStream outputStream = null;
 		try {
-			socket = new Socket("127.0.0.1", 5209); /* 当client执行完这一步时, server已经执行了inputStream.read(buf),
+			socket = new Socket("127.0.0.1", 8899); /* 当client执行完这一步时, server已经执行了inputStream.read(buf),
 												              如果这时client停在这里, 那么server一直都在read数据,
 												              表面上看server线程像是线程被阻塞了, 其实不是的, 它是代码一直在执行read操作,
 												              和正常说的线程阻塞不是一个状态, server线程没有被阻塞, 只是代码一直在执行read操作*/
-			Thread.sleep(10000);
-			outputStream = socket.getOutputStream(); 
+//			Thread.sleep(10000);
+			outputStream = socket.getOutputStream();
+			Scanner scanner = new Scanner(System.in);
+
+			for (; ; ) {
+				System.out.print("请输入:");
+				String line = scanner.nextLine();
+				if (line != null) {
+					outputStream.write(line.getBytes());
+					outputStream.flush();
+				}
+			}
 			
-			outputStream.write("hello".getBytes());
+
 			
-			outputStream.flush();
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
