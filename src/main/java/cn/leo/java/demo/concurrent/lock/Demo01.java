b30f5a9a2232e59f5f3ef45b266d8be4e45b0d3a
Youliang.Chen <cyliang1113@126.com>
2020-06-15 09:12:09 +0800

    111

--- src/main/java/cn/leo/java/demo/concurrent/lock/Demo01.java
+++ src/main/java/cn/leo/java/demo/concurrent/lock/Demo01.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * 多线程控制:
  * 1, 临界资源不能被多线程同时访问, 需要依次去访问 ,这是简单的多线程控制; (互斥)
- * 2, 多线程之间需要协调工作, 如: 生产主消费者问题; (同步)
+ * 2, 多线程之间需要协调工作, 如: 生产者消费者问题; (同步)
  */
 public class Demo01 {
     public static void main(String[] args) {
